c9c38edf56fcfdacd130be3f3721e277b2bb7b4a
Ernest Paul <ernestpaul88@gmail.com>
2022-01-27 21:34:41 -0500

    Questions and Answers

---
+++ assets/scripts/script.js
@@ -0,0 +1,60 @@
+// Best Practices----Research Quiz---
+// https://www.sitepoint.com/simple-javascript-quiz/
+
+// Questions
+var myQuestions = [
+  {
+    question: "Commonly used data types DO NOT include:_____.",
+
+    answers: {
+      1: "strings",
+      2: "booleans",
+      3: "alerts",
+      4: "numbers",
+    },
+    correctAnswer: 3,
+  },
+  {
+    question:
+      "The condition in an IF else statement is enclosed within:_______.",
+    answers: {
+      1: "quotes",
+      2: "curly brackets",
+      3: "parenthesis",
+      4: "square brackets",
+    },
+    correctAnswer: 3,
+  },
+  {
+    question: "Arrays in Javascript can be used to store:_________.",
+    answers: {
+      1: "numbers and strings",
+      2: "other arrays",
+      3: "booleans",
+      4: "all of the above",
+    },
+    correctAnswer: 4,
+  },
+  {
+    question:
+      "String values must be enclosed within:_________ when being assigned to variables.",
+    answers: {
+      1: "quotes",
+      2: "camel case",
+      3: "comma",
+      4: "question marks",
+    },
+    correctAnswer: 1,
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is :_________?",
+    answers: {
+      1: "javascript",
+      2: "terminal/bash",
+      3: "for loops",
+      4: "console.log",
+    },
+    correctAnswer: 4,
+  },
+];
